5963bbebc2057b77fe702217b8ffc721631fbde6
pareana hasoloan silalahi <151810007+hasoloan7@users.noreply.github.com>
2024-01-16 21:26:05 +0700

    Delete orange.cy.js

--- orange.cy.js
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.youtube.com/')
-    cy.get('input.ytd-searchbox').type('pesta {enter}')
-    cy.get('yt-chip-cloud-chip-renderer.yt-chip-cloud-renderer:nth-child(1) > yt-formatted-string:nth-child(1)').should('contain.text', 'All')
-  })
-})
